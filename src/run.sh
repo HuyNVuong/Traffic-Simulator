39cdd547aad89a066eb131168c57a20cf53ae1ed
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-04 20:52:12 -0600

    run.sh updated to cover all the version of python

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 # Install all the dependencies
-pip install -r requirements.txt --user
-pip install python3-tk || pip install python-tk --user # For python 2.7 user, hope no one need this :)
+pip install -r requirements.txt --user || pip3 install -r requirements.txt # If user doesn't have pip and only pip3
+pip install python3-tk || pip3 install python3-tk || pip install python-tk --user # For python 2.7 user, hope no one need this :)
 
 # Execute the Program and wala!
 python Simulation.py || python3 Simulation.py
