f4be7a7af900a9a749d9df5ba04c000bffe82fef
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-12 23:01:35 -0500

    Testing framework added

---
+++ src/SimulationTester.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+class SimulationTester(TestCase):
+    def pre_test():
+        pass
+
+    def test_func():
+        pass
+
+    def test_var():
+        pass
+
+if __name__ == "__main__":
+    st = SimulationTester()
