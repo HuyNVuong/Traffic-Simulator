2504a7b0354ae10cbf55c434bedca4c2b54f696c
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-24 23:29:53 -0500

    Revert "build 1.2"
    This reverts commit 158e46d9714cd67962dd741d362070cc1305479b.

--- src/CommandPallete.py
+++ src/CommandPallete.py
@@ -16,14 +16,18 @@ class CommandPallete(Frame):
     def create_widgets(self):
         # Starting Button
         self.start_button = Button(self)
-        self.start_button["text"] = "Start"
-        self.start_button["command"] = self.start
+        startImage = PhotoImage(file='buttonImages/StartButton.png')
+        smallerImage = startImage.subsample(14,14)
+        self.start_button.image = smallerImage
+        self.start_button = Button(self, image=smallerImage, command=self.start, height=35, width=75)
         self.start_button.grid(row=2, column=3, columnspan=2, padx=20, pady=10)
 
         # Pausing Button
         self.pause_button = Button(self)
-        self.pause_button["text"] = "Pause"
-        self.pause_button["command"] = self.pause
+        pauseImage = PhotoImage(file='buttonImages/PauseButton.png')
+        smallerImage = pauseImage.subsample(14,14)
+        self.pause_button.image = smallerImage
+        self.pause_button = Button(self, image=smallerImage, command=self.pause, height=35, width=75)
         self.pause_button.grid(row=2, column=7, columnspan=2, padx=200, pady=10)
 
         # Timer label
@@ -33,8 +37,10 @@ class CommandPallete(Frame):
 
         # Stopping Button
         self.stop_button = Button(self)
-        self.stop_button["text"] = "Stop"
-        self.stop_button["command"] = self.stop
+        stopImage = PhotoImage(file='buttonImages/StopButton.png')
+        smallerImage = stopImage.subsample(14,14)
+        self.stop_button.image = smallerImage
+        self.stop_button = Button(self, image=smallerImage, command=self.stop, height=35, width=75)
         self.stop_button.grid(row=3, column=3, columnspan=2, padx=20, pady=10)
 
         # Stepping Button
@@ -44,7 +50,11 @@ class CommandPallete(Frame):
         self.step_button.grid(row=3, column=7, columnspan=2, padx=200, pady=10)
 
         # Quit button
-        self.quit = Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit = Button(self)
+        quitImage = PhotoImage(file='buttonImages/QuitButton.png')
+        smallerImage = quitImage.subsample(14,14)
+        self.quit.image = smallerImage
+        self.quit = Button(self, image=smallerImage, command=self.master.destroy, height=35, width=75)
         self.quit.grid(row=3, column=11, columnspan=2, padx=20, pady=10)
         # self.quit.pack(side="right")
 
