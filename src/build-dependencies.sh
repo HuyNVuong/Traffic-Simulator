c472aff542cc75cbc18cad6d505bbbd12a3313b4
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-27 10:09:32 -0500

    build file updated

--- src/build-dependencies.sh
+++ src/build-dependencies.sh
@@ -5,6 +5,6 @@ if python -c 'import pkgutil; exit(not pkgutil.find_loader("tkinter"))'; then
     echo 'tkinter found!'
 else
     echo 'Finding a workaround....'
-    pip install python3-tk --user || pip3 install python3-tk || pip install python-tk --user # For python 2.7 user, hope no one need this :)
+    apt-get python3-tk # For python 2.7 user, hope no one need this :)
 fi
 
