ee014d5135b96dc1b9b750ed29b3137e4f3fae7b
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-29 14:56:16 -0500

    Tiles updated, changed class name to MapTiles to avoid conflicts

--- src/Tiles.py
+++ src/Tiles.py
@@ -1,9 +1,19 @@
 from tkinter import Canvas 
-from Raw import Point
+from Raw import Point, Tiles
 
-class Tiles(Canvas):
-    def __init__(self, master=None):
+class MapTiles(Canvas):
+    def __init__(self, state, master=None):
         super().__init__(master=master)
+        self.master = master
+        self.state = state 
+        if state == Tiles.stop_sign:
+            self.draw_stop_sign()
 
     def __repr__(self):
         return 'Tiles'
+
+    def draw_stop_sign(self):
+        pass 
+
+    def draw_traffic_lights(self):
+        pass
