ab252f6c32b8ea3df3d4237f6b3f70bd25af3b7b
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-04-01 00:10:58 -0500

    First round of test cases added

--- src/SimulationTester.py
+++ src/SimulationTester.py
@@ -1,5 +1,13 @@
-import unittest
-import pytest
+from Raw import raw_map, Tiles
+# import pytest
 
-class SimulationTester(unittest.TestCase):
-    pass
+class SimulationTester(object):
+    def pre_test(self):
+        # Test that all the entities are there 
+        assert Tiles.wall in raw_map
+        assert Tiles.road in raw_map
+        assert Tiles.car_right in raw_map
+        assert Tiles.car_left in raw_map
+        assert Tiles.car_down in raw_map
+        assert Tiles.car_up in raw_map
+        assert Tiles.traffic_lights in raw_map
