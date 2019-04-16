9d6beca1019d199586dcb360bcea9fe4073434d6
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-04-16 09:46:49 -0500

    some modifications

--- src/test_simulation_gui.py
+++ src/test_simulation_gui.py
@@ -1,9 +1,9 @@
-from CommandPallete import CommandPallete
+from Controller import Controller
 from tkinter import Tk
 from PyQt5 import QtCore, QtWidgets
 
 def test_start_button(qtbot):
     root = Tk()
-    command = CommandPallete(master=root)
+    command = Controller(master=root)
     qtbot.addWidget(command)
     qtbot.mouseClick(command.start_button, QtCore.Qt.LeftButton)
